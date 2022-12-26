99aaabe46950fd23ec1d785472b95cbcdd890d17
Arthur Ribeiro Aguiar <arthurribeiro495@gmail.com>
2022-12-26 15:18:35 -0300

    Uptade Design Section

--- scriptslider.js
+++ scriptslider.js
@@ -1,8 +1,29 @@
 const slider = document.querySelectorAll(".slider");
 const btnPrev = document.getElementById("prev-button");
 const btnNext = document.getElementById("next-button");
+const btnDesign = document.querySelectorAll(".button-design");
 
-let currentSlide = 0;
+console.log(btnDesign);
+
+const colors = [
+  "#FFFFFF",
+  "#F46E75",
+  "#168C80",
+  "#FFFFFF",
+  "#6ECEC2",
+  "#F8A52B",
+];
+
+// background: rgba(20, 36, 65, 0.315);
+
+let currentSlide = 4;
+
+slider[currentSlide].classList.add("on");
+
+// btnDesign.style.fill = colors[currentSlide];
+btnDesign.forEach((b) => {
+  b.style.fill = colors[currentSlide];
+});
 
 function hideSlider() {
   slider.forEach((item) => item.classList.remove("on"));
@@ -16,8 +37,16 @@ function nextSlider() {
   hideSlider();
   if (currentSlide === slider.length - 1) {
     currentSlide = 0;
+    // btnDesign.style.fill = colors[currentSlide];
+    btnDesign.forEach((b) => {
+      b.style.fill = colors[currentSlide];
+    });
   } else {
     currentSlide++;
+    // btnDesign.style.fill = colors[currentSlide];
+    btnDesign.forEach((b) => {
+      b.style.fill = colors[currentSlide];
+    });
   }
   showSlider();
 }
@@ -26,8 +55,16 @@ function prevSlider() {
   hideSlider();
   if (currentSlide === 0) {
     currentSlide = slider.length - 1;
+    // btnDesign.style.fill = colors[currentSlide];
+    btnDesign.forEach((b) => {
+      b.style.fill = colors[currentSlide];
+    });
   } else {
     currentSlide--;
+    // btnDesign.style.fill = colors[currentSlide];
+    btnDesign.forEach((b) => {
+      b.style.fill = colors[currentSlide];
+    });
   }
   showSlider();
 }
